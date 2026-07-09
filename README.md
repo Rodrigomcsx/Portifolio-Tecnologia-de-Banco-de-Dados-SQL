83f44c8e2a29b008acef0abebc90b897ba9b941e
Rodrigo <rodrigo@outlook.com>
2026-07-09 09:12:56 -0300

    Iniciando o ProjetoProjeto-SQL-Loja

---
+++ README.md
@@ -0,0 +1,35 @@
+# Sistema de Loja
+
+Projeto desenvolvido durante meus estudos de Banco de Dados.
+
+## Objetivo
+
+Criar um banco de dados para uma loja para controlar clientes, produtos, pedidos e itens de pedido.
+
+Aplicação de conceitos:
+
+✅ Iniciar um novo banco de dados com CREATE DATABASE
+
+✅ Criar tabelas com CREATE TABLE
+
+✅ Definir PRIMARY KEY
+
+✅ Definir FOREIGN KEY
+
+✅ Inserir dados com INSERT
+
+✅ Consultar dados com SELECT
+
+✅ Fazer um JOIN
+
+✅ Trabalhar com repositório local usando Git e repositório remoto com Git Hub
+
+
+
+
+
+## Tecnologias:
+
+- SQL
+- MySQL
+- Git e GitHub
---
+++ script.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE loja;
+
+USE loja;
+
+CREATE TABLE clientes (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE produtos (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DECIMAL(10,2)
+);
+
+CREATE TABLE pedidos (
+    id INT PRIMARY KEY,
+    id_cliente INT,
+    FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id)
+);
+
+CREATE TABLE itens_pedido (
+    id_pedido INT,
+    id_produto INT,
+    quantidade INT,
+
+    FOREIGN KEY (id_pedido)
+        REFERENCES pedidos(id),
+
+    FOREIGN KEY (id_produto)
+        REFERENCES produtos(id)
+);
+
+INSERT INTO clientes VALUES
+(1,'João'),
+(2,'Maria');
+
+INSERT INTO produtos VALUES
+(1,'Mouse',50),
+(2,'Teclado',100),
+(3,'Monitor',800);
+
+INSERT INTO pedidos VALUES
+(1,1),
+(2,2);
+
+INSERT INTO itens_pedido VALUES
+(1,1,1),
+(1,2,1),
+(2,3,1);
